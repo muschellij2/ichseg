f37eb360f718af830b729c978ff605b56c72b081
muschellij2 <muschellij2@gmail.com>
2019-02-25 12:38:22 -0500

    updated verbosity of ct_dcm2nii

--- R/ct_dcm2nii.R
+++ R/ct_dcm2nii.R
@@ -4,6 +4,7 @@
 #' @param basedir (character) directory to get files
 #' @param merge_files Should files be merged, passed do \code{\link{dcm2nii}}
 #' options
+#' @param verbose print diagnostic messages
 #' @param ... Additional parameters passed to \code{\link{dcm2nii}}
 #'
 #' @return A list or singular \code{nifti} image
@@ -11,11 +12,19 @@
 #'
 #' @importFrom dcm2niir dcm2nii check_dcm2nii
 #' @importFrom neurobase rescale_img check_nifti
-ct_dcm2nii = function(basedir = ".", merge_files = TRUE, ...) {
-  out = dcm2nii(basedir, merge_files = TRUE, ...)
+ct_dcm2nii = function(basedir = ".", merge_files = TRUE,
+                      verbose = TRUE, ...) {
+  out = dcm2nii(basedir, merge_files = TRUE, verbose = verbose,
+                ...)
   res = check_dcm2nii(out)
   img = lapply(res, function(x){
+    if (verbose) {
+      message("# reading in image")
+    }
     img = check_nifti(res)
+    if (verbose) {
+      message("# rescaling data")
+    }
     img = rescale_img(img, min.val = -1024,
                       max.val = 3071)
   })
