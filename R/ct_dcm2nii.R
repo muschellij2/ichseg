767100dac0e714f8ef2de43f026de6f9c0d78458
muschellij2 <muschellij2@gmail.com>
2025-11-18 12:15:40 -0500

    added in processing ct dcm2nii

--- R/ct_dcm2nii.R
+++ R/ct_dcm2nii.R
@@ -15,6 +15,8 @@
 #' correction process.
 #' @param fail_on_error if the exit code from [dcm2niir::dcm2nii] is not
 #' `0` and `fail_on_error = TRUE`, function will error out
+#' @param cleanup delete any temporary directories created during conversion,
+#' overrides `dcm2nii` cleanup option.
 #'
 #' @return A list or singular \code{nifti} image
 #' @export
@@ -24,6 +26,7 @@
 ct_dcm2nii = function(basedir = ".", merge_files = TRUE,
                       verbose = TRUE,
                       drop_dim = TRUE, ...,
+                      cleanup = FALSE,
                       ignore_roi_if_multiple = FALSE,
                       fail_on_error = FALSE,
                       uncorrected = FALSE) {
@@ -35,11 +38,49 @@ ct_dcm2nii = function(basedir = ".", merge_files = TRUE,
       )
     )
   }
-  out = dcm2nii(basedir, merge_files = merge_files, verbose = verbose,
+  args = list(...)
+  if (is.null(args$copy_files)) {
+    # use the default of command
+    args$copy_files = formals(dcm2niir::dcm2nii)$copy_files
+  }
+  if (cleanup && !args$copy_files) {
+    stop(
+      paste0(
+        "cleanup = TRUE requires copy_files = TRUE in dcm2nii,",
+        " otherwise original data may be deleted.."
+      )
+    )
+  }
+  out = dcm2nii(basedir,
+                merge_files = merge_files, verbose = verbose,
+                cleanup = FALSE,
                 ...)
+  img = ct_process_dcm2nii(
+    out,
+    verbose = verbose,
+    drop_dim = drop_dim,
+    cleanup = cleanup,
+    ignore_roi_if_multiple = ignore_roi_if_multiple,
+    fail_on_error = fail_on_error,
+    uncorrected = uncorrected
+  )
+  return(img)
+}
+
+#' @export
+#' @rdname ct_dcm2nii
+#' @param out output from [ct_dcm2nii]
+ct_process_dcm2nii = function(out,
+                              verbose = TRUE,
+                              drop_dim = TRUE,
+                              cleanup = FALSE,
+                              ignore_roi_if_multiple = FALSE,
+                              fail_on_error = FALSE,
+                              uncorrected = FALSE) {
   if (fail_on_error && out$result > 0) {
     stop("Error in result from dcm2nii and fail_on_error = TRUE")
   }
+  dir_temp = attr(out$result, "copy_files_temporary_directory")
   res = check_dcm2nii(out, ignore_roi_if_multiple = ignore_roi_if_multiple,
                       uncorrected = uncorrected)
   names(res) = res
@@ -58,6 +99,14 @@ ct_dcm2nii = function(basedir = ".", merge_files = TRUE,
   if (length(res) == 1) {
     img = img[[1]]
     names(img) = res
+  }
+  if (cleanup) {
+    if (verbose) {
+      message("# cleaning up temporary files")
+    }
+    if (!is.null(dir_temp) && dir.exists(dir_temp)) {
+      unlink(dir_temp, recursive = TRUE, force = TRUE)
+    }
   }
   attr(img, "dcm2nii_result") = out$result
   attr(img, "files") = res
